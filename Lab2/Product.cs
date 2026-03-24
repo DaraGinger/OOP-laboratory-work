aa5531c0ffe37a6367d0d3aee6889938494c982f
DaraGinger <darinafrouk@gmail.com>
2026-03-24 15:15:49 +0200

    work 2

---
+++ Lab2/Product.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab2
+{
+// реалізувати такі методи:
+//- два конструктори -без параметрів і з параметрами для ініціалізації об'єкту;
+//- методи доступу до полів класу(властивості не використовувати);
+//- метод формування рядка з інформацією про об'єкт;
+//- методи класу для обробки значень полів об'єкта відповідно до індивідуального
+//варіанту.
+
+// 12
+// Продукт
+// Калорійність 100 г продукту
+//Вага продукту в грамах
+//1.Обчислити загальну калорійність продукту.
+//2. Обчислити, яку масу продукту можна з’їсти на задану кількість калорій.
+
+    public class Product
+    {
+        public Product()
+        {
+        }
+
+        public Product(int calories, int weightGrams)
+        {
+            CaloriesIn100Grams = calories;
+            WeightGrams = weightGrams;
+        }
+
+        private int CaloriesIn100Grams;
+
+        private int WeightGrams;
+
+        public void SetCaloriesIn100Grams(int calories)
+        {
+            CaloriesIn100Grams = calories;
+        }
+
+        public void SetWeightGrams(int weightGrams)
+        {
+            WeightGrams = weightGrams;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"Calories per 100g: {CaloriesIn100Grams} calories\nWeight in grams: {WeightGrams} grams");
+        }
+
+        public void CalculateCaloriesByWeight()
+        {
+            int calories = CaloriesIn100Grams * WeightGrams / 100;
+            Console.WriteLine($"{WeightGrams}g of product contains {calories} calories");
+        }
+
+        public void CalculateWeightByCalories(int calories)
+        {
+            int weight = calories * 100 / CaloriesIn100Grams;
+
+            Console.WriteLine($"{calories} calories is {weight} grams of product.");
+        }
+    }
+}
---
+++ Lab2/Program.cs
@@ -0,0 +1,34 @@
+﻿//В головній програмі необхідно:
+//- створити 2-3 об’єкти класу, використовуючи різні конструктори;
+//- вводити значення полів кожного об’єкта з клавіатури;
+//- протестувати роботу всіх методів класу;
+//- вивести на екран результати виклику методів класу.
+
+using Lab2;
+
+int calories, weightGrams;
+Console.WriteLine("Enter calories of the first product per 100 grams:");
+int.TryParse(Console.ReadLine(), out calories);
+Console.WriteLine("Enter weight of of the first product  in grams:");
+int.TryParse(Console.ReadLine(), out weightGrams);
+
+Product product1 = new Product();
+product1.SetCaloriesIn100Grams(calories);
+product1.SetWeightGrams(weightGrams);
+product1.Print();
+product1.CalculateCaloriesByWeight();
+product1.CalculateWeightByCalories(1200);
+
+Console.WriteLine(new string ('-', 50));
+
+Console.WriteLine("Enter calories of the second product per 100 grams:");
+int.TryParse(Console.ReadLine(), out calories);
+
+Console.WriteLine("Enter weight of of the second product  in grams:");
+int.TryParse(Console.ReadLine(), out weightGrams);
+
+Product product2 = new Product(120, 1300);
+product2.Print();
+product2.CalculateCaloriesByWeight();
+product2.CalculateWeightByCalories(550);
+
